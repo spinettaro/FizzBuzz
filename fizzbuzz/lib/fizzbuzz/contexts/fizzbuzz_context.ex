bf4ca96e983f0a10c8b12f4658577064b415affb
ds <d.spinetti@craneware.com>
2021-02-07 11:55:11 +0000

    added fizzbuzz_context respecting the context pattern from Elixir/Phoenix guideline - https://hexdocs.pm/phoenix/contexts.html added logic for fizzbuzz algorithm added relative tests in TDD approach to cover logic and range boundaries

---
+++ fizzbuzz/lib/fizzbuzz/contexts/fizzbuzz_context.ex
@@ -0,0 +1,29 @@
+defmodule Fizzbuzzweb.FizzbuzzContext do
+
+  @fizz     "Fizz"
+  @buzz     "Buzz"
+  @fizzbuzz @fizz<>@buzz
+
+  @fizz_divisor 3
+  @buzz_divisor 5
+
+  @to_limit 100_000_000_000
+
+  def fizzbuzz(from, to) when is_integer(from) and is_integer(from) and to <= @to_limit do
+    Enum.map(from..to, fn number ->
+      fizz = rem(number, @fizz_divisor) == 0
+      buzz = rem(number, @buzz_divisor) == 0
+      cond do
+        fizz and buzz -> @fizzbuzz
+        fizz          -> @fizz
+        buzz          -> @buzz
+        true          -> number
+      end
+    end)
+  end
+
+  def fizzbuzz(from, to) do
+    raise "The passed range #{from}..#{to} doesn't respect the criteria: `from` and `to` must be integers and `to` must be less than #{@to_limit}"
+  end
+
+end
---
+++ fizzbuzz/test/fizzbuzz/fizzbuzz_context_test.exs
@@ -0,0 +1,55 @@
+defmodule Fizzbuzzweb.FizzbuzzContextTest do
+  alias Fizzbuzzweb.FizzbuzzContext
+
+  use ExUnit.Case
+
+  test "Given range 1..3 will return 1,2,Fizz" do
+    # ARRANGE
+    from=1
+    to=3
+    # ACT
+    result= FizzbuzzContext.fizzbuzz(from, to)
+    # ASSERT
+    expected= [1, 2, "Fizz"]
+    assert( result ==  expected)
+  end
+
+  test "Given range 4..5 will return only 4,Buzz" do
+    # ARRANGE
+    from=4
+    to=5
+    # ACT
+    result= FizzbuzzContext.fizzbuzz(from, to)
+    # ASSERT
+    expected= [4, "Buzz"]
+    assert( result ==  expected)
+  end
+
+  test "Given range 10..15 will return Fizz Buzz and FizzBuzz" do
+    # ARRANGE
+    from=10
+    to=15
+    # ACT
+    result= FizzbuzzContext.fizzbuzz(from, to)
+    # ASSERT
+    expected= ["Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+    assert( result ==  expected)
+  end
+
+  test "Given a range with no integers will raise an exception" do
+    # ARRANGE
+    from="a"
+    to="b"
+    # ACT and ASSERT
+    assert_raise(RuntimeError, "The passed range #{from}..#{to} doesn't respect the criteria: `from` and `to` must be integers and `to` must be less than 100000000000", fn -> FizzbuzzContext.fizzbuzz(from, to) end)
+  end
+
+  test "Given a range over the limit will raise an exception" do
+    # ARRANGE
+    from=0
+    to=100000000000 + 1
+    # ACT and ASSERT
+    assert_raise(RuntimeError, "The passed range #{from}..#{to} doesn't respect the criteria: `from` and `to` must be integers and `to` must be less than 100000000000", fn -> FizzbuzzContext.fizzbuzz(from, to) end)
+  end
+
+end
