87bcccbe9e0f8bfacfd78f67742d010c511a36a4
ds <d.spinetti@craneware.com>
2021-02-07 14:54:28 +0000

    refactored fizzbuzzcontext removing default params and making scan_params private as suggested from warnings

--- fizzbuzz/lib/fizzbuzz/contexts/fizzbuzz_context.ex
+++ fizzbuzz/lib/fizzbuzz/contexts/fizzbuzz_context.ex
@@ -15,7 +15,7 @@ defmodule Fizzbuzz.FizzbuzzContext do
     raise "The passed params doesn't respect the criteria: you must insert a page and a size -> %{\"page\" => page, \"page_size\" => page_size}"
   end
 
-  def paged_fizzbuzz( params \\ %{}) do
+  def paged_fizzbuzz( params) do
     %{"page" => page, "page_size" => size}= scan_params( params)
     from= ( (page - 1) * size) + 1
     to= from - 1 + size
@@ -47,7 +47,7 @@ defmodule Fizzbuzz.FizzbuzzContext do
     raise "The passed range #{from}..#{to} doesn't respect the criteria: `from` and `to` must be integers and `to` must be less than #{@to_limit}"
   end
 
-  def scan_params( params) do
+  defp scan_params( params) do
     Map.merge(@default_params, params)
     |> Map.update( "page", 1, fn el -> if is_bitstring( el), do: String.to_integer( el), else: el end)
     |> Map.update( "page_size", 1000, fn el -> if is_bitstring( el), do: String.to_integer( el), else: el end)
