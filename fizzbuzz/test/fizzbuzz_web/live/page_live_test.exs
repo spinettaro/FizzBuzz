672dfe2ea292d53e718b80ff921fc49bfe28d029
ds <d.spinetti@craneware.com>
2021-02-07 11:42:15 +0000

    fixed page_live test after the welcome test has changed to be coherent with FizzBuzz project added test for fizzbuzz_live to make sure the redirect happen after the button click

--- fizzbuzz/test/fizzbuzz_web/live/page_live_test.exs
+++ fizzbuzz/test/fizzbuzz_web/live/page_live_test.exs
@@ -5,7 +5,14 @@ defmodule FizzbuzzWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "Welcome to FizzBuzz!"
+    assert render(page_live) =~ "Welcome to FizzBuzz!"
   end
+
+  test "click on let's start", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+    render_click(view, :start)
+    assert_redirected view, "/fizzbuzz"
+  end
+
 end
