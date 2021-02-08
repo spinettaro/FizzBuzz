ddd047599d27fa2291a7a160bf8e14051ee6c2fb
ds <d.spinetti@craneware.com>
2021-02-08 22:40:40 +0000

    changed page and page_size to integer

--- fizzbuzz_client/CommandLineU.pas
+++ fizzbuzz_client/CommandLineU.pas
@@ -9,22 +9,22 @@ type
     TCommandLine = class(TObject)
     private
         FCommand:  String;
-        FPageSize: String;
-        FPage:     String;
+        FPageSize: Integer;
+        FPage:     Integer;
         FNumber: Integer;
         FFavourite: Integer;
         procedure SetCommand(const Value: String);
-        procedure SetPage(const Value: String);
-        procedure SetPageSize(const Value: String);
+        procedure SetPage(const Value: Integer);
+        procedure SetPageSize(const Value: Integer);
         procedure SetNumber(const Value: Integer);
         procedure SetFavourite(const Value: Integer);
     public
         [CLPName('c'), CLPLongName('Command'),CLPDescription('API to call, can be "fizzbuzz" or "favourites"'),CLPRequired]
         property Command: String read FCommand write SetCommand;
         [CLPName('p'), CLPLongName('Page'), CLPDescription('Page number in a pagination context. Valid only for "fizbuzz" command')]
-        property Page: String read FPage write SetPage;
+        property Page: Integer read FPage write SetPage;
         [CLPName('ps'), CLPLongName('PageSize'),CLPDescription('How many elements the API has to return per page. Valid only for "fizzbuzz" command')]
-        property PageSize: String read FPageSize write SetPageSize;
+        property PageSize: Integer read FPageSize write SetPageSize;
         [CLPName('n'), CLPLongName('Number'), CLPDescription('A number you want to mark as favourite. Valid only for "favourites" command')]
         property Number: Integer read FNumber write SetNumber;
         [CLPName('f'), CLPLongName('Favourite'), CLPDescription('An integer 1 or 0 to mark the number as favourite. Valid only for "favourites" command')]
@@ -51,12 +51,12 @@ begin
   FNumber := Value;
 end;
 
-procedure TCommandLine.SetPage(const Value: String);
+procedure TCommandLine.SetPage(const Value: Integer);
 begin
     FPage := Value;
 end;
 
-procedure TCommandLine.SetPageSize(const Value: String);
+procedure TCommandLine.SetPageSize(const Value: Integer);
 begin
     FPageSize := Value;
 end;
