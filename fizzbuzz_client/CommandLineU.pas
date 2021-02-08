ed1018a3047ab5a098a0e21a6964de9b346d056c
ds <d.spinetti@craneware.com>
2021-02-08 22:24:38 +0000

    added CommandLine object to represent the parameters that a user can pass to the CLI added GpCommandLineParser as helper for CLI operations added implementation of error handling in case of bad params

---
+++ fizzbuzz_client/CommandLineU.pas
@@ -0,0 +1,64 @@
+unit CommandLineU;
+
+interface
+
+uses
+    GpCommandLineParser;
+
+type
+    TCommandLine = class(TObject)
+    private
+        FCommand:  String;
+        FPageSize: String;
+        FPage:     String;
+        FNumber: Integer;
+        FFavourite: Integer;
+        procedure SetCommand(const Value: String);
+        procedure SetPage(const Value: String);
+        procedure SetPageSize(const Value: String);
+        procedure SetNumber(const Value: Integer);
+        procedure SetFavourite(const Value: Integer);
+    public
+        [CLPName('c'), CLPLongName('Command'),CLPDescription('API to call, can be "fizzbuzz" or "favourites"'),CLPRequired]
+        property Command: String read FCommand write SetCommand;
+        [CLPName('p'), CLPLongName('Page'), CLPDescription('Page number in a pagination context. Valid only for "fizbuzz" command')]
+        property Page: String read FPage write SetPage;
+        [CLPName('ps'), CLPLongName('PageSize'),CLPDescription('How many elements the API has to return per page. Valid only for "fizzbuzz" command')]
+        property PageSize: String read FPageSize write SetPageSize;
+        [CLPName('n'), CLPLongName('Number'), CLPDescription('A number you want to mark as favourite. Valid only for "favourites" command')]
+        property Number: Integer read FNumber write SetNumber;
+        [CLPName('f'), CLPLongName('Favourite'), CLPDescription('An integer 1 or 0 to mark the number as favourite. Valid only for "favourites" command')]
+        property Favourite: Integer read FFavourite write SetFavourite;
+
+    end;
+
+implementation
+
+{ TCommandLine }
+
+procedure TCommandLine.SetCommand(const Value: String);
+begin
+    FCommand := Value;
+end;
+
+procedure TCommandLine.SetFavourite(const Value: Integer);
+begin
+  FFavourite := Value;
+end;
+
+procedure TCommandLine.SetNumber(const Value: Integer);
+begin
+  FNumber := Value;
+end;
+
+procedure TCommandLine.SetPage(const Value: String);
+begin
+    FPage := Value;
+end;
+
+procedure TCommandLine.SetPageSize(const Value: String);
+begin
+    FPageSize := Value;
+end;
+
+end.
--- fizzbuzz_client/fizzbuzz_cli.dpr
+++ fizzbuzz_client/fizzbuzz_cli.dpr
@@ -5,7 +5,9 @@
 
 uses
     System.SysUtils,
-    Console in 'Console.pas';
+    Console in 'Console.pas',
+    CommandLineU in 'CommandLineU.pas',
+    GpCommandLineParser in 'GpCommandLineParser.pas';
 
 procedure Logo;
 begin
@@ -31,15 +33,40 @@ begin
     NormVideo;
 end;
 
+var
+    cl:     TCommandLine;
+    parsed: boolean;
+
 begin
     Logo;
     TextColor(LightGreen);
+
+    cl := TCommandLine.Create;
     try
-    { TODO -oUser -cConsole Main : Insert code here }
-        readln;
-    except
-        on E: Exception do
-            Writeln(E.ClassName, ': ', E.Message);
+        try
+            parsed := CommandLineParser.Parse(cl);
+        except
+            on E: ECLPConfigurationError do
+            begin
+                Writeln('*** Configuration error ***');
+                Writeln(Format('%s, position = %d, name = %s',
+                    [E.ErrorInfo.Text, E.ErrorInfo.Position,
+                    E.ErrorInfo.SwitchName]));
+                Exit;
+            end;
+        end;
+        if not parsed then
+        begin
+            Writeln(Format('%s, position = %d, name = %s',
+                [CommandLineParser.ErrorInfo.Text,
+                CommandLineParser.ErrorInfo.Position,
+                CommandLineParser.ErrorInfo.SwitchName]));
+            Writeln;
+            for var ls in CommandLineParser.Usage do
+              Writeln( ls);
+        end
+    finally
+        cl.Free;
     end;
 
 end.
