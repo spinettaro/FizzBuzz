5183fa76ca4449c5651d47d9694bc4f8aad3b8bb
ds <d.spinetti@craneware.com>
2021-02-09 17:30:46 +0000

    added command as constants formatted code

--- fizzbuzz_client/CommandLineU.pas
+++ fizzbuzz_client/CommandLineU.pas
@@ -5,13 +5,19 @@ interface
 uses
     GpCommandLineParser;
 
+const
+    FizzbuzzCommand = 'fizzbuzz';
+    FavouritesCommand = 'favourites';
+    ValidCommands: TArray<String> = [FizzbuzzCommand, FavouritesCommand];
+
 type
+
     TCommandLine = class(TObject)
     private
-        FCommand:  String;
-        FPageSize: Integer;
-        FPage:     Integer;
-        FNumber: Integer;
+        FCommand:   String;
+        FPageSize:  Integer;
+        FPage:      Integer;
+        FNumber:    Integer;
         FFavourite: Integer;
         procedure SetCommand(const Value: String);
         procedure SetPage(const Value: Integer);
@@ -19,20 +25,38 @@ type
         procedure SetNumber(const Value: Integer);
         procedure SetFavourite(const Value: Integer);
     public
-        [CLPName('c'), CLPLongName('Command'),CLPDescription('API to call, can be "fizzbuzz" or "favourites"'),CLPRequired]
+        [CLPName('c'), CLPLongName('Command'),
+            CLPDescription('API to call, can be "fizzbuzz" or "favourites"'),
+            CLPRequired]
         property Command: String read FCommand write SetCommand;
-        [CLPName('p'), CLPLongName('Page'), CLPDescription('Page number in a pagination context. Valid only for "fizbuzz" command')]
+        [CLPName('p'), CLPLongName('Page'),
+            CLPDescription
+            ('Page number in a pagination context. Valid only for "fizbuzz" command')
+            ]
         property Page: Integer read FPage write SetPage;
-        [CLPName('ps'), CLPLongName('PageSize'),CLPDescription('How many elements the API has to return per page. Valid only for "fizzbuzz" command')]
+        [CLPName('s'), CLPLongName('PageSize'),
+            CLPDescription
+            ('How many elements the API has to return per page. Valid only for "fizzbuzz" command')
+            ]
         property PageSize: Integer read FPageSize write SetPageSize;
-        [CLPName('n'), CLPLongName('Number'), CLPDescription('A number you want to mark as favourite. Valid only for "favourites" command')]
+        [CLPName('n'), CLPLongName('Number'),
+            CLPDescription
+            ('A number you want to mark as favourite. Valid only for "favourites" command')
+            ]
         property Number: Integer read FNumber write SetNumber;
-        [CLPName('f'), CLPLongName('Favourite'), CLPDescription('An integer 1 or 0 to mark the number as favourite. Valid only for "favourites" command')]
+        [CLPName('f'), CLPLongName('Favourite'),
+            CLPDescription
+            ('An integer 1 or 0 to mark the number as favourite. Valid only for "favourites" command')
+            ]
         property Favourite: Integer read FFavourite write SetFavourite;
 
     end;
 
 implementation
+
+uses
+    System.StrUtils,
+    System.SysUtils;
 
 { TCommandLine }
 
@@ -43,12 +67,12 @@ end;
 
 procedure TCommandLine.SetFavourite(const Value: Integer);
 begin
-  FFavourite := Value;
+    FFavourite := Value;
 end;
 
 procedure TCommandLine.SetNumber(const Value: Integer);
 begin
-  FNumber := Value;
+    FNumber := Value;
 end;
 
 procedure TCommandLine.SetPage(const Value: Integer);
